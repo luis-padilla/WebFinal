8ef749cdb5830a1075925023e524296d5278e96f
luis-padilla <33160159+luis-padilla@users.noreply.github.com>
2018-03-10 15:43:53 -0700

    HoverFix

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 var image = document.getElementById("slideShowImage");
 var imageArray =  ["images/spaceMinerals/rock1.jpg",'images/spaceMinerals/rock2.jpg','images/spaceMinerals/rock3.jpg','images/spaceMinerals/rock4.jpg','images/spaceMinerals/rock5.jpg','images/spaceMinerals/rock6.jpg'];
 var index = 0;
+
 function slideShow(){
-    
     image.setAttribute("src",imageArray[index]);
     index++;
     if(index >= imageArray.length){
@@ -11,11 +11,19 @@ function slideShow(){
 }
 
 window.onload = function(){
-    setInterval(slideShow, 2500);
+
+    setInterval(slideShow, 10000);
+
+    $("#intro").hover(
+        function(){
+            $("#intro").css("font-size","24px");
+            console.log("over");
+        },
+        function(){
+            $("#intro").css("font-size","12px");
+            console.log("out");
+        });
+
+    
 }
 
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this)("text-font: 16px")
-    });
-});
